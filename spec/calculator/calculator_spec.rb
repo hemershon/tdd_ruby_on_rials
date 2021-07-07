1607b3fb74e4826fadb8f3bb2a643121fc0cab39
Hemershon Silva <hemershon@gmail.com>
2021-07-06 22:21:54 -0300

    test

---
+++ spec/calculator/calculator_spec.rb
@@ -0,0 +1,18 @@
+require 'calculator'
+describe Calculator do 
+    it 'sum method for 2 numbers' do 
+        calc = Calculator.new
+        result = calc.sum(5, 7)
+        expect(result).to eq(12)
+
+    end
+
+    it 'use sum method for 2 nimbers with negative numbers' do 
+        calc = Calculator.new
+        result = calc.sum(5, 7)
+        expect(result).to eq(12)
+
+    end
+
+
+end
